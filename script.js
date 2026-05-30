9b661be907fac08e34c6798c474b6f23da660c14
Chetra <sopheapsochetra@gmail.com>
2026-05-30 22:35:49 +0700

    New Lesson

---
+++ script.js
@@ -0,0 +1,6 @@
+const name = 'Sopheap Sochetra';
+const age = 19;
+const course = 'Computer Science';
+let lessonNumber = 1;
+const msg = `Hi, I am ${name}, a ${age}, years old ${course} student. Currently on lesson ${lessonNumber}.`
+console.log(msg);
